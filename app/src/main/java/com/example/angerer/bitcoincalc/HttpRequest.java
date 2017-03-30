d56e61cc29c583b86209f7d38573faf4b5af9153
Angerer <simon@angererfamily.de>
2017-03-30 19:01:02 +0200

    Test

--- app/src/main/java/com/example/angerer/bitcoincalc/HttpRequest.java
+++ app/src/main/java/com/example/angerer/bitcoincalc/HttpRequest.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class HttpRequest {
@@ -32,12 +30,6 @@ public class HttpRequest {
             // String response
             InputStream in = new BufferedInputStream(conn.getInputStream());
             req_response = getStream(in);
-        }
-        catch (MalformedURLException e) {
-            Log.e(LOGGIN_TAG, e.getMessage());
-        }
-        catch (ProtocolException e) {
-            Log.e(LOGGIN_TAG, e.getMessage());
         }
         catch (IOException e) {
             Log.e(LOGGIN_TAG, e.getMessage());
